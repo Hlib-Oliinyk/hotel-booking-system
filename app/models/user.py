2437c53e90b8bdd865db0ed5720a59559822c7ef
Олійник Гліб <glib070707@gmail.com>
2026-04-13 14:20:48 +0300

    feat: add User model with roles

---
+++ app/models/user.py
@@ -0,0 +1,18 @@
+import enum
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.database import Base
+
+
+class UserRole(str, enum.Enum):
+    ADMIN = "admin"
+    CUSTOMER = "customer"
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    email: Mapped[str] = mapped_column(unique=True, nullable=False, index=True)
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
+    role: Mapped[UserRole] = mapped_column(default=UserRole.CUSTOMER)
